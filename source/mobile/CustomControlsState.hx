78ba8b438d45c3266ec62ca257444c6b8145f040
GaboWuz ★ <96131201+Luisanimegame@users.noreply.github.com>
2025-10-22 16:00:52 -0300

    real

--- source/mobile/CustomControlsState.hx
+++ source/mobile/CustomControlsState.hx
@@ -98,7 +98,7 @@ class CustomControlsState extends MusicBeatSubstate
 
 		var savebutton = new FlxUIButton((exitbutton.x + exitbutton.width + 25),25,"exit and save",() -> {
 			save();
-			FlxG.switchState(new OptionsMenu());
+			FlxG.switchState(new ConfigMenu());
 		});
 		savebutton.resize(250,50);
 		savebutton.setLabelFormat("VCR OSD Mono",24,FlxColor.BLACK,"center");
