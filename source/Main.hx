17862f372fe04e0f1aa419e7aa9f7bed77c0b54d
GaboWuz ★ <96131201+Luisanimegame@users.noreply.github.com>
2025-10-21 23:38:41 -0300

    não tendi

--- source/Main.hx
+++ source/Main.hx
@@ -7,27 +7,27 @@ import hxcodec.VideoHandler;
 
 class Main extends Sprite
 {
-	public static var fpsDisplay:FPS;
-	public static var preload:Bool = !Sys.args().contains("-nopreload");
+    public static var fpsDisplay:FPS;
+    public static var preload:Bool = !Sys.args().contains("-nopreload");
 
-	public function new()
-	{
-		super();
+    public function new()
+    {
+        super();
 
-		#if CORY
-		if(preload)
-			addChild(new FlxGame(0, 0, CoryState, 1, 144, 144, true));
-		else
-			addChild(new FlxGame(0, 0, CoryState, 1, 144, 144, true));
-		#else
-		if(preload)
-			addChild(new FlxGame(0, 0, Startup, 1, 144, 144, true));
-		else
-			addChild(new FlxGame(0, 0, TitleVidState, 1, 144, 144, true));
-		#end
-			
-		fpsDisplay = new FPS(10, 3, 0xFFFFFF);
-		fpsDisplay.visible = false;
-		addChild(fpsDisplay);
-	}
+        #if CORY
+        if(preload)
+            addChild(new FlxGame(0, 0, CoryState, true, 144, 144, true));
+        else
+            addChild(new FlxGame(0, 0, CoryState, true, 144, 144, true));
+        #else
+        if(preload)
+            addChild(new FlxGame(0, 0, Startup, true, 144, 144, true)); // Linha 24 corrigida
+        else
+            addChild(new FlxGame(0, 0, TitleVidState, true, 144, 144, true)); // Linha 26 corrigida
+        #end
+            
+        fpsDisplay = new FPS(10, 3, 0xFFFFFF);
+        fpsDisplay.visible = false;
+        addChild(fpsDisplay);
+    }
 }
---
+++ source/Paths.hx
@@ -0,0 +1,19 @@
+package;
+
+import flixel.FlxG;
+import flixel.graphics.frames.FlxAtlasFrames;
+import openfl.utils.AssetType;
+import openfl.utils.Assets as OpenFlAssets;
+
+class Paths // fiquei surpreso
+{
+	inline static public function video(key:String)
+	{
+		return Asset2File.getPath('assets/videos/$key.mp4');
+	}
+
+	inline static public function getSparrowAtlas(key:String, ?library:String)
+	{
+		return FlxAtlasFrames.fromSparrow(image(key, library), file('images/$key.xml', library));
+	}
+}
