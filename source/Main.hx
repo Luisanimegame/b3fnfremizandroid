d956c76e35273bbf35ebad2f12c6a20647498216
GaboWuz ★ <96131201+Luisanimegame@users.noreply.github.com>
2025-10-22 00:45:01 -0300

    Realmente, tendi nada

--- source/Main.hx
+++ source/Main.hx
@@ -8,25 +8,14 @@ import hxcodec.VideoHandler;
 class Main extends Sprite
 {
     public static var fpsDisplay:FPS;
-    public static var preload:Bool = !Sys.args().contains("-nopreload");
-    public static var video:Bool = true; // Nova variável para controlar vídeos
+    public static var video:Bool = true;
 
     public function new()
     {
         super();
 
-        #if CORY
-        if(preload)
-            addChild(new FlxGame(0, 0, CoryState, 144, 144, true, true));
-        else
-            addChild(new FlxGame(0, 0, CoryState, 144, 144, true, true));
-        #else
-        if(preload)
-            addChild(new FlxGame(0, 0, Startup, 144, 144, true, true));
-        else
-            addChild(new FlxGame(0, 0, TitleVidState, 144, 144, true, true));
-        #end
-            
+        addChild(new FlxGame(0, 0, TitleVidState, 144, 144, true, true));
+        
         fpsDisplay = new FPS(10, 3, 0xFFFFFF);
         fpsDisplay.visible = false;
         addChild(fpsDisplay);
